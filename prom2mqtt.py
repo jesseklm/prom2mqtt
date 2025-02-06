be6a37da2edccd4811467b5aca501e14c010fa9d
Jesse Kleemann <j.kleemann@tu-bs.de>
2025-02-06 22:57:18 +0100

    clean exit

--- prom2mqtt.py
+++ prom2mqtt.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import signal
 import time
 from asyncio import sleep
 
@@ -10,7 +11,7 @@ from prometheus_client.parser import text_string_to_metric_families
 
 from config import get_first_config
 
-__version__ = '0.0.4'
+__version__ = '0.0.6'
 
 
 class Prom2Mqtt:
@@ -26,7 +27,7 @@ class Prom2Mqtt:
                 logging.warning(f'unknown logging level: %s.', logging_level)
         self.update_rate: int = self.config.get('update_rate', 60)
 
-        will_message: Message = Message(f'{self.config["mqtt_topic"]}available', 'offline', will_delay_interval=10,
+        will_message: Message = Message(f'{self.config["mqtt_topic"]}available', 'offline', will_delay_interval=5,
                                         retain=True)
         self.client: MQTTClient = MQTTClient(client_id=None, will_message=will_message)
         self.client.on_connect = self.on_connect
@@ -44,18 +45,21 @@ class Prom2Mqtt:
             logging.error(f"{self.config['mqtt_server']=}, {e=}")
         return False
 
+    async def loop_iteration(self) -> None:
+        await self.connect_mqtt()
+        for scraper in self.config['scrapers']:
+            for family in text_string_to_metric_families(await self.fetch(scraper['exporter_url'])):
+                if family.name in scraper['filters']:
+                    for sample in family.samples:
+                        labels = '_'.join(f'{label}_{value}' for label, value in sample.labels.items())
+                        logging.debug("Name: {0} Labels: {1} Value: {2}".format(*sample))
+                        if self.client.is_connected or await self.connect_mqtt():
+                            self.client.publish(f'{self.config["mqtt_topic"]}{sample.name}_{labels}', sample.value)
+
     async def loop(self) -> None:
         while True:
             start_time: float = time.perf_counter()
-            await self.connect_mqtt()
-            for scraper in self.config['scrapers']:
-                for family in text_string_to_metric_families(await self.fetch(scraper['exporter_url'])):
-                    if family.name in scraper['filters']:
-                        for sample in family.samples:
-                            labels = '_'.join(f'{label}_{value}' for label, value in sample.labels.items())
-                            logging.debug("Name: {0} Labels: {1} Value: {2}".format(*sample))
-                            if self.client.is_connected or await self.connect_mqtt():
-                                self.client.publish(f'{self.config["mqtt_topic"]}{sample.name}_{labels}', sample.value)
+            await self.loop_iteration()
             time_taken: float = time.perf_counter() - start_time
             time_to_sleep: float = self.update_rate - time_taken
             logging.debug('looped in %.2fms, sleeping %.2fs.', time_taken * 1000, time_to_sleep)
@@ -65,6 +69,11 @@ class Prom2Mqtt:
     def on_connect(self, client, flags, rc, properties):
         client.publish(f'{self.config["mqtt_topic"]}available', 'online', retain=True)
         logging.info('mqtt connected.')
+
+    async def exit(self):
+        if self.client.is_connected:
+            await self.client.disconnect(reason_code=4)
+            logging.info('mqtt disconnected.')
 
     @staticmethod
     async def fetch(url: str) -> str:
@@ -81,7 +90,25 @@ class Prom2Mqtt:
 
 async def main():
     prom2mqtt = Prom2Mqtt()
-    await prom2mqtt.loop()
+    loop = asyncio.get_running_loop()
+    main_task = asyncio.current_task()
+
+    def shutdown_handler():
+        if not main_task.done():
+            main_task.cancel()
+
+    try:
+        for sig in (signal.SIGINT, signal.SIGTERM):
+            loop.add_signal_handler(sig, shutdown_handler)
+    except NotImplementedError:
+        pass
+    try:
+        await prom2mqtt.loop()
+    except asyncio.CancelledError:
+        logging.info('exiting.')
+    finally:
+        await prom2mqtt.exit()
+        logging.info('exited.')
 
 
 if __name__ == '__main__':
